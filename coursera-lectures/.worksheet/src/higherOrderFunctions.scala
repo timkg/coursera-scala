b0ac1f3c45c4abf42893f5cfe638afdd76b36601
Tim Koch-Grunberg <tim.theodor@gmail.com>
2012-10-24 01:57:50 +0100

    changed project name

---
+++ coursera-lectures/.worksheet/src/higherOrderFunctions.scala
@@ -0,0 +1,3 @@
+object higherOrderFunctions {import scala.runtime.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(73); 
+  println("Welcome to the Scala worksheet")}
+}
---
+++ coursera-lectures/bin/week3/higherOrderFunctions.sc
@@ -0,0 +1,3 @@
+object higherOrderFunctions {
+  println("Welcome to the Scala worksheet")       //> Welcome to the Scala worksheet
+}
